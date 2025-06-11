a349a5799ca1bdb89b2da70040eba61769761efd
rachraines <rachraines97@gmail.com>
2025-06-11 11:38:32 -0400

    create_server_connection updated to be create_db_connection

--- create_db_connection.py
+++ create_db_connection.py
@@ -1,13 +1,15 @@
 import mysql.connector
 from mysql.connector import Error
+import pandas as pd
 
-def create_server_connection(host_name, user_name, user_password):
+def create_db_connection(host_name, user_name, user_password, db_name):
     connection = None
     try:
         connection = mysql.connector.connect(
             host = host_name,
             user = user_name,
-            passwd = user_password
+            passwd = user_password,
+            database = db_name
         )
         print("MySQL Database connection successful")
     except Error as err:
@@ -15,4 +17,7 @@ def create_server_connection(host_name, user_name, user_password):
 
     return connection
 
-connection = create_server_connection("local host", "root", pw)
+pw = "992277Rf!"
+db = "school"
+
+connection = create_db_connection("localhost", "root", pw)
