230bc8eafb1d50c55e01eba5b875ef1cd296b339
rachraines <rachraines97@gmail.com>
2025-06-17 10:23:48 -0400

    client, participant, and course tables created in school database

--- main.py
+++ main.py
@@ -73,6 +73,39 @@ CREATE TABLE IF NOT EXISTS teacher (
 );
 """
 
+create_client_table = """
+CREATE TABLE client(
+    client_id INT PRIMARY KEY,
+    client_name VARCHAR(40) NOT NULL,
+    address VARCHAR(60) NOT NULL,
+    inddustry VARCHAR(20)
+    );
+    """
+
+create_participant_table = """
+CREATE TABLE participant(
+participant_id INT PRIMARY KEY,
+first_name VARCHAR(40) NOT NULL,
+last_name VARCHAR(40) NOT NULL,
+phone_no VARCHAR(20),
+client INT
+);
+"""
+
+create_course_table = """
+CREATE TABLE course(
+course_id INT PRIMARY KEY,
+course_name VARCHAR(40) NOT NULL,
+language VARCHAR(3) NOT NULL,
+level VARCHAR(2),
+course_length_weeks INT,
+start_date DATE,
+in_school BOOLEAN,
+teacher INT,
+client INT
+);
+"""
+
 
 def main():
     # Connect to MySql server (no DB yet)
@@ -89,6 +122,9 @@ def main():
     
     # Create tables
     execute_query(db_connection, create_teacher_table)
+    execute_query(db_connection, create_client_table)
+    execute_query(db_connection, create_participant_table)
+    execute_query(db_connection, create_course_table)
 
     db_connection.close()
     print("All setup completed successfully.")
