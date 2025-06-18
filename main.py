4a41ca055a6714e92d19a6e49d3ec80314760f00
rachraines <rachraines97@gmail.com>
2025-06-18 10:15:13 -0400

    tables populated with data

--- main.py
+++ main.py
@@ -141,6 +141,70 @@ CREATE TABLE takes_course(
 );
 """
 
+# Populate client table
+pop_client = """
+INSERT INTO client VALUES
+(101, 'Big Business Federation', '123 Falschungstraße, 10999 Berlin', 'NGO'),
+(102, 'eCommerce GmbH', '27 Ersatz Allee, 10317 Berlin', 'Retail'),
+(103, 'AutoMaker AG',  '20 Künstlichstraße, 10023 Berlin', 'Auto'),
+(104, 'Banko Bank',  '12 Betrugstraße, 12345 Berlin', 'Banking'),
+(105, 'WeMoveIt GmbH', '138 Arglistweg, 10065 Berlin', 'Logistics');
+"""
+
+# Populate participant table
+pop_participant = """
+INSERT INTO participant VALUES
+(101, 'Marina', 'Berg','491635558182', 101),
+(102, 'Andrea', 'Duerr', '49159555740', 101),
+(103, 'Philipp', 'Probst',  '49155555692', 102),
+(104, 'René',  'Brandt',  '4916355546',  102),
+(105, 'Susanne', 'Shuster', '49155555779', 102),
+(106, 'Christian', 'Schreiner', '49162555375', 101),
+(107, 'Harry', 'Kim', '49177555633', 101),
+(108, 'Jan', 'Nowak', '49151555824', 101),
+(109, 'Pablo', 'Garcia',  '49162555176', 101),
+(110, 'Melanie', 'Dreschler', '49151555527', 103),
+(111, 'Dieter', 'Durr',  '49178555311', 103),
+(112, 'Max', 'Mustermann', '49152555195', 104),
+(113, 'Maxine', 'Mustermann', '49177555355', 104),
+(114, 'Heiko', 'Fleischer', '49155555581', 105);
+"""
+
+# Populate course table
+pop_course = """
+INSERT INTO course VALUES
+(12, 'English for Logistics', 'ENG', 'A1', 10, '2020-02-01', TRUE,  1, 105),
+(13, 'Beginner English', 'ENG', 'A2', 40, '2019-11-12',  FALSE, 6, 101),
+(14, 'Intermediate English', 'ENG', 'B2', 40, '2019-11-12', FALSE, 6, 101),
+(15, 'Advanced English', 'ENG', 'C1', 40, '2019-11-12', FALSE, 6, 101),
+(16, 'Mandarin für Autoindustrie', 'MAN', 'B1', 15, '2020-01-15', TRUE, 3, 103),
+(17, 'Français intermédiaire', 'FRA', 'B1',  18, '2020-04-03', FALSE, 2, 101),
+(18, 'Deutsch für Anfänger', 'DEU', 'A2', 8, '2020-02-14', TRUE, 4, 102),
+(19, 'Intermediate English', 'ENG', 'B2', 10, '2020-03-29', FALSE, 1, 104),
+(20, 'Fortgeschrittenes Russisch', 'RUS', 'C1',  4, '2020-04-08',  FALSE, 5, 103);
+"""
+
+# Populate takes_course table
+pop_takescourse = """
+INSERT INTO takes_course VALUES
+(101, 15),
+(101, 17),
+(102, 17),
+(103, 18),
+(104, 18),
+(105, 18),
+(106, 13),
+(107, 13),
+(108, 13),
+(109, 14),
+(109, 15),
+(110, 16),
+(110, 20),
+(111, 16),
+(114, 12),
+(112, 19),
+(113, 19);
+"""
 
 def main():
     # Connect to MySql server (no DB yet)
@@ -165,6 +229,12 @@ def main():
     execute_query(db_connection, alter_course_again)
     execute_query(db_connection, create_takescourse_table)
 
+    # Populate tables
+    execute_query(db_connection, pop_client)
+    execute_query(db_connection, pop_participant)
+    execute_query(db_connection, pop_course)
+    execute_query(db_connection,pop_takescourse)
+    
     db_connection.close()
     print("All setup completed successfully.")
 
